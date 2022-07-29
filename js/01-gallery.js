4ef09654c776a83aa8f54777aac1e5879b1dce07
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-07-29 15:48:16 +0300

    Task 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from "basiclightbox";
 // Change code below this line
+// ${original}
 
-console.log(galleryItems);
+const galleryImages = document.querySelector(".gallery");
+const imageColections = createImagesColection(galleryItems);
+galleryImages.insertAdjacentHTML("beforeend",imageColections);
+
+function createImagesColection(items) {
+
+ return  items.map(({preview,description,original }) => {
+         return `<div class="gallery__item">
+          <a 
+          class="gallery__link"
+           href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            data-source="${original}" 
+            alt="${description}" />
+          </a>
+        </div>`;
+
+
+ }
+     
+).join('')
+    
+}
+
+
+
+
+galleryImages.addEventListener('click', onImageClick)
+
+function onImageClick(e) {
+    
+        e.preventDefault();
+    if (e.target.nodeName!=='IMG') {
+      return;
+    }
+  const imgModal = e.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+        <img src="${imgModal}" width="800" height="600">
+    `);
+   
+    instance.show();
+}
+
+
+
+document.addEventListener("keyup", function (e) {  
+    if (e.key !== "Escape") {
+      return;
+  } if (document.querySelector(".basicLightbox--visible")) {
+       document
+        .querySelector(".basicLightbox--visible")
+        .classList.remove("basicLightbox--visible");
+    } else {
+      return
+    }
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImages = document.querySelector(".gallery");
+const imageColections = createImagesColection(galleryItems);
+galleryImages.insertAdjacentHTML("beforeend", imageColections);
+
+function createImagesColection(items) {
+  return items
+    .map(({ preview, description, original }) => {
+        return `<a class="gallery__item"
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+galleryImages.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.on()
+
+  
+}
